2669650c82caff0184dddd7481b98b99936ca3fc
Ackerman <maksimligaj151@gmail.com>
2025-01-28 01:00:44 +0500

    Fix for list

--- CarRentalProj/main.cpp
+++ CarRentalProj/main.cpp
@@ -53,11 +53,12 @@ struct RentalOrder {
 class RentalService
 {
 private:
-    vector<Car> available_cars;
+    list<Car> available_cars;
     queue<pair<Customer, pair<double, double>>> rental_requests;
     double distance(pair<double, double> loc1, pair<double, double> loc2) {
         return sqrt(pow(loc1.first - loc2.first, 2) + pow(loc1.second - loc2.second, 2));
     }
+
 public:
     
 };
