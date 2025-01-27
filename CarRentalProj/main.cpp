7b11467a27705528ea37897e8cf938406d225943
Ackerman <maksimligaj151@gmail.com>
2025-01-27 13:49:56 +0500

    Car struct

--- CarRentalProj/main.cpp
+++ CarRentalProj/main.cpp
@@ -6,6 +6,24 @@
 
 using namespace std;
 
+struct Car {
+    int car_id;
+    string mark_c;
+    string model;
+    double price_per_day;
+    vector<string> features;
+    pair<double, double> location;
+
+    Car(int id, string mk, string mdl, double price, vector<string> f_s, pair<double, double> loc) {
+        car_id = id;
+        mark_c = mk;
+        model = mdl;
+        price_per_day = price;
+        features = f_s;
+        location = loc;
+    }
+};
+
 int main() {
     std::cout << "Hello, World!\n";
     return 0;
