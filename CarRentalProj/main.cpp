e7cba15737f4017948e29deda7a95dbec3608ec6
Ackerman <maksimligaj151@gmail.com>
2025-01-28 01:03:28 +0500

    Comment

--- CarRentalProj/main.cpp
+++ CarRentalProj/main.cpp
@@ -59,7 +59,7 @@ private:
     double distance(pair<double, double> loc1, pair<double, double> loc2) {
         return sqrt(pow(loc1.first - loc2.first, 2) + pow(loc1.second - loc2.second, 2));
     }
-
+//Danik tupoi
 public:
     
 };
