3933cb24b4c460759a4d739fa4884bfdf5d27519
Ackerman <maksimligaj151@gmail.com>
2025-01-27 13:36:17 +0500

    Initial Commit

---
+++ CarRentalProj/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CarRentalProj
+//
+//  Created by Max Ligay on 27.01.2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
