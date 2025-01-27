95e21a8b9c3070aca2ee604523c09129b2049d7d
Ackerman <maksimligaj151@gmail.com>
2025-01-27 13:40:18 +0500

    libraries, base

--- CarRentalProj/main.cpp
+++ CarRentalProj/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <vector>
+#include <queue>
+#include <cmath>
+#include <algorithm>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
+using namespace std;
+
+int main() {
     std::cout << "Hello, World!\n";
     return 0;
 }
